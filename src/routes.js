d7bf8202e35fa6401fc3f35e080e5039d34dfe9d
helipe09 <luishelipe@gmail.com>
2020-12-03 18:11:24 -0300

    map-svg

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home/home';
 import Login from './Pages/Login/login';
 import Header from './Components/Header/header';
+import Teste from './Pages/Teste/teste';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/teste" component={Teste} />
       </Switch>
     </BrowserRouter>
   )
