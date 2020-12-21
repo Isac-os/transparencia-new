804893ab46edf441267918693e64a7aa015cfd72
helipe09 <luishelipe@gmail.com>
2020-12-21 16:51:45 -0300

    dashboard

--- src/Services/api.jsx
+++ src/Services/api.jsx
@@ -2,18 +2,12 @@ import axios from 'axios';
 
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'https://api-isac-transparencia.herokuapp.com'
-  // baseURL: 'https://staging-api-portal.herokuapp.com'
-  ,
-=======
   //baseURL: 'https://api-isac-transparencia.herokuapp.com',
   //baseURL: 'https://staging-api-portal.herokuapp.com',
   baseURL: 'https://portal-isac-core.herokuapp.com',
   headers: {
     "content-type": "application/json;charset=utf-8",
   },
->>>>>>> feature/dashboard
 });
 
 export default api;
